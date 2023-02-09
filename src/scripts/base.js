a89d7060c7ec6d54c9d1db1adf012df773bd73d4
acr0matic <work.acr0matic@gmail.com>
2023-02-09 21:25:49 +0300

    feat: мобильное меню

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -2,3 +2,13 @@ const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
 const isMobile = window.matchMedia('(max-width: 576px)').matches;
 const isTablet = window.matchMedia('(max-width: 991px)').matches;
+
+const StyleСlass = {
+  'header': {
+    'background': 'header--background',
+  },
+
+  'mobile': {
+    'open': 'mobile-menu--open',
+  },
+}
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -13,7 +13,23 @@ if (header) {
   }
 
   window.addEventListener('scroll', () => {
-    if (window.scrollY >= contentHeight) header.classList.add('header--background')
-    else header.classList.remove('header--background')
+    if (window.scrollY >= contentHeight) header.classList.add(StyleСlass.header.background)
+    else header.classList.remove(StyleСlass.header.background)
   });
+
+
+
+  const mobile = document.querySelector('.mobile-menu');
+  const mobileOverlay = document.querySelector('.mobile-menu__overlay');
+
+  const mobileBurger = header.querySelector('.hamburger');
+
+  mobileBurger.addEventListener('click', () => Menu());
+  mobileOverlay.addEventListener('click', () => Menu());
+
+  function Menu() {
+    mobile.classList.toggle(StyleСlass.mobile.open);
+    mobileBurger.classList.toggle('is-active')
+    document.body.classList.toggle('disable-scroll');
+  }
 }
